50fbe504e8ee3056d970df45bbdeb24b46aee078
Gabriel Gonzalez <gabrielgonzgils@gmail.com>
2024-12-09 06:02:00 +0100

    create pagination

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -30,7 +30,10 @@ const PostController = {
   },
   async getAll(req, res) {
     try {
-      const posts = await Post.find();
+      const { page, limit } = req.query;
+      const posts = await Post.find()
+        .limit(limit)
+        .skip((page - 1) * limit);
       res.status(200).send(posts);
     } catch (error) {
       console.error(error);
