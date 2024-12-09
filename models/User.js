4a8c886eeb69ce8ba19d56a07ba3ee46e72d2bf8
Gabriel Gonzalez <gabrielgonzgils@gmail.com>
2024-12-09 04:47:20 +0100

    add authentication to 3 endpoints of post

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     name: String,
     email: String,
@@ -11,6 +11,6 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const router = express.Router()
-const PostController = require('../controllers/PostController');
+const express = require("express");
+const router = express.Router();
+const PostController = require("../controllers/PostController");
+const { authentication } = require("../middlewares/authentication");
 
-router.post('/create',PostController.create)
-router.put('/update/id/:_id',PostController.update)
-router.delete('/delete/id/:_id',PostController.delete)
-router.get('/getAll',PostController.getAll)
-router.get('/getByTitle/:title',PostController.getByTitle)
-router.get('/getById/:_id',PostController.getById)
+router.post("/create", authentication, PostController.create);
+router.put("/update/id/:_id", authentication, PostController.update);
+router.delete("/delete/id/:_id", authentication, PostController.delete);
+router.get("/getAll", PostController.getAll);
+router.get("/getByTitle/:title", PostController.getByTitle);
+router.get("/getById/:_id", PostController.getById);
 
 module.exports = router;
