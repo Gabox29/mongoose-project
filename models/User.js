d05b2528da4c0f78d46da5160a1fe27cd82ae053
Gabriel Gonzalez <gabrielgonzgils@gmail.com>
2024-12-09 04:33:19 +0100

    install JWT, create authentication functionality

--- models/User.js
+++ models/User.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     name: String,
     email: String,
     password: String,
-    birthday: Date
-}, { timestamps: true });
+    birthday: Date,
+    tokens: [],
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
